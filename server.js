6319ea40b329084dbdf939ac783c635ee822517c
mc0918 <mcampbell0918@gmail.com>
2019-04-03 11:20:32 -0500

    set path

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-module.exports = function(app) {
-  var path = require("path");
-  var app = require("express");
+var express = require("express");
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"));
-  });
-};
+var app = express();
+
+var path = require("path");
+
+var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
